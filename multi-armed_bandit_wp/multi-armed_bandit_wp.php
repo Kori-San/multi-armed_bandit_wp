9d4497a85c45778ae0e03056693c8dd003a19996
Kori-San <aymen@ezzayer.net>
2023-06-25 01:09:04 +0200

    [ADD] Pseudo-code

--- multi-armed_bandit_wp/multi-armed_bandit_wp.php
+++ multi-armed_bandit_wp/multi-armed_bandit_wp.php
@@ -7,4 +7,15 @@
    Author: LE ROY Andy & EZZAYER Aymen
    License: MIT
    */
+
+   /* 
+      IU: - Rajouter des templates dans une liste 
+          - Rentrer le nombre de tests à effectuer en Exploration
+          
+      Algorithme: - Quand on accéde à une nouvelle page on choisis un template en fonction du Bandit Manchot
+                  - Quand un utilisateur clique sur un boutton / form / ect... on augmente la Reward de 1
+                  - Quand l'utilisateur quitte la page on met à jour le poid et taux d'apparations des templates
+                  - Apres n tests on rentre en exploitation et on affiche que le template qui "marche"
+                  - Après 9 x n visites sur les pages on repasse en exploration
+   */
 ?>
