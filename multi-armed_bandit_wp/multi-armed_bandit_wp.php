64ed2a0574e2de6dfcc3739e4c292c44d79008fb
Kori-San <aymen@ezzayer.net>
2023-06-25 00:40:41 +0200

    [INIT] Docker and WP Plugin

---
+++ multi-armed_bandit_wp/multi-armed_bandit_wp.php
@@ -0,0 +1,10 @@
+<?php
+   /*
+   Plugin Name: Multi Armed Bandit WordPress
+   description: >-
+  A Plugin that applies the Multi-Armed Bandit Algorithm into WordPress
+   Version: 1.0
+   Author: LE ROY Andy & EZZAYER Aymen
+   License: MIT
+   */
+?>
