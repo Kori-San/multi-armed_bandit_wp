59e3096ada722fd4e3e4bb9062eea65d1b2e703c
Kori-San <aymen@ezzayer.net>
2023-06-25 02:44:25 +0200

    [ADD] Feat Settings menu

--- multi-armed_bandit_wp/multi-armed_bandit_wp.php
+++ multi-armed_bandit_wp/multi-armed_bandit_wp.php
@@ -1,21 +1,11 @@
 <?php
    /*
    Plugin Name: Multi Armed Bandit WordPress
-   description: >-
-  A Plugin that applies the Multi-Armed Bandit Algorithm into WordPress
+   description: A Plugin that applies the Multi-Armed Bandit Algorithm to WordPress Templates
    Version: 1.0
    Author: LE ROY Andy & EZZAYER Aymen
    License: MIT
    */
 
-   /* 
-      IU: - Rajouter des templates dans une liste 
-          - Rentrer le nombre de tests à effectuer en Exploration
-          
-      Algorithme: - Quand on accéde à une nouvelle page on choisis un template en fonction du Bandit Manchot
-                  - Quand un utilisateur clique sur un boutton / form / ect... on augmente la Reward de 1
-                  - Quand l'utilisateur quitte la page on met à jour le poid et taux d'apparations des templates
-                  - Apres n tests on rentre en exploitation et on affiche que le template qui "marche"
-                  - Après 9 x n visites sur les pages on repasse en exploration
-   */
+   include "multi-armed_bandit_wp_settings.php";
 ?>
